08abea0e89bb2d176804e8e0afdcc20e25e2506e
Cade <kisercade@gmail.com>
2025-03-17 18:26:07 -0400

    Beginnings: Test Data Load/Save

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using OOP_Project.Utils;
+
+namespace OOP_Project
+{
+    internal class Program
+    {
+        //Whether debug data is printed to console
+        public static bool debug = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting data...");
+            String data = DataManagement.Load();
+            Console.WriteLine("Data: " + data);
+            Console.WriteLine("Input new data: ");
+            data = Console.ReadLine();
+            DataManagement.Save(data);
+            Console.WriteLine("New data: " + data);
+        }
+    }
+}
---
+++ Utils/DataManagement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project.Utils
+{
+    public class DataManagement
+    {
+        static String defaulter = "default";
+        public static void Save(String data)
+        {
+            if(Program.debug)
+                Console.WriteLine("Saving data to " + Directory.GetCurrentDirectory() + "/firearmData.txt");
+            File.WriteAllText(Directory.GetCurrentDirectory() + "/firearmData.txt", data);
+        }
+        public static String Load()
+        {
+            if (Program.debug)
+                Console.WriteLine("Loading data from " + Directory.GetCurrentDirectory() + "/firearmData.txt");
+            try
+            {
+                return File.ReadAllText(Directory.GetCurrentDirectory() + "/firearmData.txt");
+            }
+            catch (Exception e)
+            {
+                return defaulter;
+            }
+        }
+    }
+}
